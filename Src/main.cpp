72d298af867ab0f5ab774eafd05904d2603cd3f5
valiullin_fi <vfi@eleps.ru>
2025-04-04 09:55:26 +0300

    Add FreeRTOS and lvgl-9.2.2

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,3 +1,15 @@
+#include <cstdio>
+#include <cstring>
+
+#include "stm32f7xx.h"
+/*
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+*/
+
+/*
 #include "main.h"
 #include "cmsis_os.h"
 #include "adc.h"
@@ -19,12 +31,11 @@
 #include "usb_host.h"
 #include "gpio.h"
 #include "fmc.h"
-
+*/
 #include "uart_log.h"
 
 extern "C" void SystemClock_Config(void);
 extern "C" void PeriphCommonClock_Config(void);
-extern "C" void MX_FREERTOS_Init(void);
 
 int main() {
     HAL_Init();
@@ -32,7 +43,7 @@ int main() {
     SystemClock_Config();
     PeriphCommonClock_Config();
 
-    MX_GPIO_Init();
+    //MX_GPIO_Init();
     //MX_ADC3_Init();
     //MX_CRC_Init();
     //MX_DCMI_Init();
@@ -54,7 +65,7 @@ int main() {
     //MX_TIM5_Init();
     //MX_TIM8_Init();
     //MX_TIM12_Init();
-    MX_USART1_UART_Init();   // Virtual COM Port
+    //MX_USART1_UART_Init();   // Virtual COM Port
     //MX_USART6_UART_Init();
     //MX_FATFS_Init();
 
@@ -66,7 +77,110 @@ int main() {
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
     HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
 
-    MX_FREERTOS_Init();
-    osKernelStart();
-    while (1) {}
+    //MX_FREERTOS_Init();
+    //osKernelStart();
+    while (1) {
+        GPIOI->ODR ^= GPIO_ODR_OD1;
+        HAL_Delay(1000);
+    }
+}
+
+extern "C" void Error_Handler(void) {
+    __disable_irq();
+    while (1) {
+    }
+}
+
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /** Configure LSE Drive Capability
+    */
+    HAL_PWR_EnableBkUpAccess();
+
+    /** Configure the main internal regulator output voltage
+    */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 25;
+    RCC_OscInitStruct.PLL.PLLN = 400;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 8;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+
+    /** Activate the Over-Drive mode
+    */
+    if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+        Error_Handler();
+    }
+
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_6) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+/**
+  * @brief Peripherals Common Clock Configuration
+  * @retval None
+  */
+void PeriphCommonClock_Config(void) {
+    RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+    /** Initializes the peripherals clock
+    */
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC | RCC_PERIPHCLK_SAI2
+                                               | RCC_PERIPHCLK_SDMMC1 | RCC_PERIPHCLK_CLK48;
+    PeriphClkInitStruct.PLLSAI.PLLSAIN = 384;
+    PeriphClkInitStruct.PLLSAI.PLLSAIR = 5;
+    PeriphClkInitStruct.PLLSAI.PLLSAIQ = 2;
+    PeriphClkInitStruct.PLLSAI.PLLSAIP = RCC_PLLSAIP_DIV8;
+    PeriphClkInitStruct.PLLSAIDivQ = 1;
+    PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_8;
+    PeriphClkInitStruct.Sai2ClockSelection = RCC_SAI2CLKSOURCE_PLLSAI;
+    PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48SOURCE_PLLSAIP;
+    PeriphClkInitStruct.Sdmmc1ClockSelection = RCC_SDMMC1CLKSOURCE_CLK48;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+    /* USER CODE BEGIN Callback 0 */
+
+    /* USER CODE END Callback 0 */
+    if (htim->Instance == TIM6) {
+        HAL_IncTick();
+    }
+    /* USER CODE BEGIN Callback 1 */
+
+    /* USER CODE END Callback 1 */
 }
---
+++ Src/sources.cmake
@@ -0,0 +1,39 @@
+# Рекурсивно находим все .cpp и .c файлы в папке Src и её подкаталогах
+file(GLOB_RECURSE SOURCES
+        ${CMAKE_CURRENT_LIST_DIR}/*.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/*.c
+)
+
+# Исключаем указанные файлы
+set(EXCLUDE_FILES
+        ${CMAKE_CURRENT_LIST_DIR}/syscalls.c
+        ${CMAKE_CURRENT_LIST_DIR}/sysmem.c
+        ${CMAKE_CURRENT_LIST_DIR}/eth/eth.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/twi/i2c.c
+)
+
+foreach (file ${EXCLUDE_FILES})
+    list(REMOVE_ITEM SOURCES ${file})
+endforeach ()
+
+# Добавляем найденные исходники в target_sources
+target_sources(${CMAKE_PROJECT_NAME} PUBLIC ${SOURCES})
+
+# Путь к папке Src
+set(SRC_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+# Рекурсивно находим все директории в папке Src и её подкаталогах
+file(GLOB_RECURSE ALL_DIRS LIST_DIRECTORIES TRUE ${SRC_DIR}/*)
+
+# Добавляем только директории
+set(INCLUDE_DIRS "")
+foreach(item ${ALL_DIRS})
+    if(IS_DIRECTORY ${item})
+        list(APPEND INCLUDE_DIRS ${item})
+    endif()
+endforeach()
+
+# Добавляем папки в target_include_directories
+target_include_directories(${CMAKE_PROJECT_NAME} PUBLIC ${INCLUDE_DIRS})
+
+add_compile_definitions(-DDEBUG)
